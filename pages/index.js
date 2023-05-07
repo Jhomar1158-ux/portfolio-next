9eaf367fb82a9037358cdf48232123a0f02c554e
Jhomar1158-ux <jhomar.astuyauri.h>
2023-05-07 02:53:44 -0500

    wip: :construction: MAIN add responsive navbar and first section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import {Container, Box, Heading} from '@chakra-ui/react';
+import Image from 'next/image';
 
 const Page = () => {
     return(
@@ -12,6 +13,16 @@ const Page = () => {
                         Jhomar Astuyauri
                     </Heading>
                     <p>Frontend/Product/Design</p>
+                </Box>
+                <Box flexShrink={0} mt={{base:4, md:0}} ml={{md:6}} align='center' position="relative" width="100px" height="100px" borderRadius="full" overflow="hidden">
+                    <Box position="absolute" top="0" left="0" right="0" bottom="0">
+                        <Image borderColor='whiteAlpha.800' borderWidth={2} borderStyle="solid" display="inline-block" borderRadius="full"
+                        height="100"
+                        width="100" 
+                        src="/images/JhomarConsultor.png"
+                        alt='Profile image'
+                        />
+                    </Box>
                 </Box>
             </Box>
         </Container>
